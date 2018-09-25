efb1cad35bdf80d58c0c0e2ab37a7885168abae6
Khari Woods <khariwoods3@gmail.com>
2018-09-25 19:19:09 -0400

    2nd commit. Fixed Navigation bar issue partially. Button click needs to be fixed. Website to be presented to client.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 // animate drop down content
 // select elements
 var dropDownBtn = $('.dropdown-btn');
+var $icon = $('i');
 var dropDownContent = $('.dropdown-content');
+
+
+
 dropDownBtn.on('click',function(){
 	dropDownContent.slideToggle(500,'swing');
+	// Toggle class
+	$icon.toggleClass('fa-bars fa-times')
 })
+
+
+// Screen Width
+var $window = $(window);
+if($window.width()>768){
+	dropDownContent.css({'display':'none'})
+}
+
+
